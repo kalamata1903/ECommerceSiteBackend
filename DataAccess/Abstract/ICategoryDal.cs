4699afc720e710a1f388ba08b1dac7a786043553
kalamata1903 <mehmet1903ozyurek@gmail.com>
2021-05-10 18:09:13 +0300

    Generic repository design pattern

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -1,5 +1,4 @@
-﻿using Entities.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal
+    public interface ICategoryDal
     {
-        
+
     }
 }
-
